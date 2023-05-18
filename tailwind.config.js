aaa4fe13388ee3320154440961a3aed10dab7c31
mauricio-js <mauricio.upwork@omnicode.solutions>
2023-05-18 19:48:47 +0200

    add fonts

---
+++ tailwind.config.js
@@ -0,0 +1,58 @@
+/** @type {import('tailwindcss').Config} */
+
+export const content = ['./src/**/*.{js,jsx,ts,tsx}'];
+export const theme = {
+  screens: {
+    'vs': '330px',
+    'ns': '430px',
+    'sm': '640px',
+    'md': '768px',
+    'lg': '1024px',
+    'xl': '1280px',
+    '2xl': '1536px',
+  },
+  fontFamily: {
+    sans: ['General Sans', 'sans-serif'],
+  },
+  container: {
+    center: true,
+  },
+  extend: {
+    maxWidth: {
+      100: '430px',
+    },
+    padding: {
+      31: '31px',
+      42: '42px',
+    },
+    margin: {
+      15: '15px',
+      18: '18px',
+      100: '100px',
+      120: '120px',
+    },
+    borderRadius: {
+      10: '10px',
+      20: '20px',
+    },
+    colors: {
+      green: {
+        10: '#78E98C',
+        20: '#0F2110',
+        30: '#19301A',
+        40: '#122413',
+        70: '#061807',
+        80: '#122413',
+      },
+      gray: {
+        10: '#737373',
+      },
+      dark: '#242731',
+      shark: '#1B1D21',
+      bombay: '#B2B3BD',
+      alabaster: '#F7F7F7',
+      flowerblue: '#5274EE',
+    },
+  },
+};
+export const plugins = [];
